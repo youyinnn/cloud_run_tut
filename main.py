8ed3c4d9afbcfa7e8ece463fe92d28a2b9eb0649
youyinnn <youyinnn@gmail.com>
2023-08-29 17:42:22 -0400

    print request;

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ app = Flask(__name__)
 def hello_world():
     """Example Hello World route."""
 
-    print("Example Hello World route.")
     print(request.method)
+    print(request.data)
     
     return f"Hello World!!!@@@!!!"
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
